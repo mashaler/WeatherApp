0bf76b4cc900206792fa1632b53338e7a15afd2d
mashaler <mashalerolva@gmail.com>
2024-09-09 15:26:48 +0200

    Intial commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Rolva Mashale on 2024/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
